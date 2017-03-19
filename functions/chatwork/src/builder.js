9af3851b3b83a2bf82bc808a00a3aa2b792e7e01
Nate-River56 <hydrangea56@me.com>
2017-03-20 02:34:07 +0900

    Revert builder.js

--- functions/chatwork/src/builder.js
+++ functions/chatwork/src/builder.js
@@ -2,16 +2,18 @@ import fs from 'fs';
 import handlebars from 'handlebars';
 
 let build = (eventName, payload) =>{
-  return Promise.denodeify(fs.readFile)
-    .call(fs, './templates/' + eventName + '.hbs')
-    .then((source)=>{
-      return Promise.resolve(handlebars.compile(source))
-    })
-    .then((template)=>{
-      const msg = template(payload);
-      console.log(msg);
-      return Promise.resolve(msg);
-    });
+  const source = fs.readFileSync(
+    './templates/' + eventName +'.hbs',
+    'utf-8'
+  );
+  console.log(source);
+
+  const template = handlebars.compile(source);
+
+  const message = template(payload);
+  console.log(message);
+
+  return message;
 }
 
 export default build;
