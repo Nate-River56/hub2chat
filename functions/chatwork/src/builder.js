b77332509a55e86f8eb7fedc0bf880da4763accb
Nate-River56 <hydrangea56@me.com>
2017-03-20 02:27:41 +0900

    Add libraries

---
+++ functions/chatwork/src/builder.js
@@ -0,0 +1,17 @@
+import fs from 'fs';
+import handlebars from 'handlebars';
+
+let build = (eventName, payload) =>{
+  return Promise.denodeify(fs.readFile)
+    .call(fs, './template/' + eventName + '.hbs')
+    .then((source)=>{
+      return Promise.resolve(handlebars.compile(source))
+    })
+    .then((template)=>{
+      const msg = template(payload);
+      console.log(msg);
+      return Promise.resolve(msg);
+    });
+}
+
+export default build;
