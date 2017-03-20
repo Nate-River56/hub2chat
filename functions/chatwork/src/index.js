304d429f7e1e43459a360a2f07034b288fe2fe9d
Nate-River56 <hydrangea56@me.com>
2017-03-20 14:16:09 +0900

    Update index.js

--- functions/chatwork/src/index.js
+++ functions/chatwork/src/index.js
@@ -1,44 +1,43 @@
-import template from './builder.js';
-import kms from './kms.js';
-import cw from './post.js';
+let template = require('./builder.js');
+let kms = require('./kms.js');
+let cw = require('./post.js');
 
 exports.handler = (event, context, callback) => {
 
   console.log('Received event:', JSON.stringify(event, null, 2));
 
+  // try {
+  //   var config = require('./.env.json')
+  //   for (var key in config) {
+  //     process.env[key] = config[key]
+  //   }
+  // } catch (err) {
+  //   // ignore
+  // }
+
   // Encrypted Chatwork token
   const enc_cwtoken = process.env.CHATWORK_API_TOKEN;
 
-  // Original token
-  const enc_token = process.env.TOKEN;
-
   // Chatwork Room ID from queryString
-  const room_id = event.pathParameters.room_id;
+  const room_id = event.pathParameters['room_id'];
 
   // Payload from GitHub
   const payload = event.requestParameters;
 
+  // GitHub Event Name
+  const eventName = event.headers['X-GitHub-Event'];
 
-  kms.decrypt(enc_token).then((token)=>{
-    return new Promise((resolve, reject)=>{
-      if (token != event.queryStrings.token){
-        reject("Authentication Failed.");
+  Promise.all([
+    kms.decrypt(enc_cwtoken),
+    new Promise((resolve, reject)=>{
+      if(room_id != ''){
+        resolve(room_id);
+      }else{
+        reject("room_id is blank.");
       }
-      resolve(token);
-    });
-  }).then(()=>{
-    return Promise.all([
-      kms.decrypt(enc_cwtoken),
-      new Promise((resolve, reject)=>{
-        if(!room_id){
-          resolve(room_id);
-        }else{
-          reject("room_id is blank.");
-        }
-      }),
-      template.build(eventName, payload),
-    ]);
-  }).then((payload)=>{
+    }),
+    template.build(eventName, payload)
+  ]).then((payload)=>{
     return cw.message(
       payload[0], // Raw Chatwork token
       payload[1], // Chatwork Room ID
@@ -46,11 +45,11 @@ exports.handler = (event, context, callback) => {
     );
   }).then((text)=>{
     callback(null, {"text": text});
-    context.done();
+    context.succeed();
   }).catch((reason)=>{
     console.log(reason);
     console.log("Not post to chatwork.");
-    context.done();
+    context.fail();
   });
 
 }
