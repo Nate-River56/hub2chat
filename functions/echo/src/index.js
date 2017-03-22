a11084d04de5cabfc49a9cdaa9761e135f051e6f
Nate-River56 <hydrangea56@me.com>
2017-03-22 13:52:04 +0900

    Autofix using eslint

--- functions/echo/src/index.js
+++ functions/echo/src/index.js
@@ -1,7 +1,7 @@
 export default (event, context, callback) => {
   console.log('Received event:', JSON.stringify(event, null, 2));
-  callback(null, event, function(){
+  callback(null, event, () => {
     context.done();
   });
-}
+};
 
